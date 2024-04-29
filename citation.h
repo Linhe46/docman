2a8bf74b549239af42559898f4fb4ff4e73588e8
huaxuefeng <979747379@qq.com>
2024-04-29 12:58:48 +0800

    complete citation_load

--- citation.h
+++ citation.h
@@ -13,13 +13,29 @@ enum Type
 
 class Citation
 {
+    std::string id;
     Type type;
+
+public:
+    Citation() {}
+    Citation(std::string id_, Type type_) : id(id_), type(type_) {}
 };
-class Citation_web
-{
-    std::string s;
+class Book : public Citation{
+    std::string isbn;
+public:
+    Book(std::string id_, Type type_, std::string isbn_) : Citation(id_, type_), isbn(isbn_) {}
 };
-class Article : public Citation
-{
+class Webpage : public Citation{
+    std::string url;
+public:
+    Webpage(std::string id_, Type type_, std::string url_) : Citation(id_, type_), url(url_) {}
+};
+class Article : public Citation{
+    std::string author,title,journal,year,volume,issue;
+public:
+    Article(std::string id_,Type type_,std::string author_,std::string title_,
+    std::string journal_,std::string year_,std::string volume_,std::string issue_):
+    Citation(id_,type_),author(author_),title(title_),journal(journal_),year(year_),
+    volume(volume_),issue(issue_){}
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -5,9 +5,35 @@
 #include "citation.h"
 #include "utils.hpp"
 
+#include "third_parties/nlohmann/json.hpp"
+#include "third_parties/cpp-httplib/httplib.h"
+
 std::vector<Citation *> loadCitations(const std::string &filename)
 {
     // FIXME: load citations from file
+    std::vector<Citation*>c;
+    std::ifstream file{filename};
+    nlohmann::json data=nlohmann::json::parse(file);
+    for(auto& item:data["citations"]){
+        auto id=item["id"].get<std::string>();
+        auto type=item["type"].get<std::string>();
+        Citation* cite;
+        if(type=="book")
+                cite=new Book(id,book,item["isbn"].get<std::string>());
+        else if(type=="webpage")
+                cite=new Book(id,webpage,item["url"].get<std::string>());
+        else if(type=="article"){
+            auto author=item["author"].get<std::string>();
+            auto title=item["title"].get<std::string>();
+            auto journal=item["journal"].get<std::string>();
+            auto year=std::to_string(item["year"].get<int>());
+            auto volume=item["volume"].get<std::string>();
+            auto issue=item["issue"].get<std::string>();
+            cite=new Article(id,article,author,title,journal,year,volume,issue);
+        }
+        c.push_back(cite);
+    }
+    return c;
 }
 
 int main(int argc, char **argv)
