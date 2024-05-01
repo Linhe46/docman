f29f9ce19b99d4d81a70acc123b66ccedf4a02b9
huaxuefeng <979747379@qq.com>
2024-05-02 00:07:07 +0800

    repair non_exist input 2

--- main.cpp
+++ main.cpp
@@ -64,6 +64,8 @@ std::vector<Citation *> loadCitations(const std::string &filename)
 }
 std::string readFromFile(const std::string& filename){
     std::ifstream file(filename);
+    if(!file.is_open())
+        std::exit(1);
     std::string s;
     char x;
     while(file.get(x))
